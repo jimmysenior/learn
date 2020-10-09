6adbe1b3f90994b75f7f37f4ca460442275b6ebf
290084098@qq.com <290084098@qq.com>
2020-10-09 23:34:01 +0800

    Add learn-concurrent

---
+++ learn-effective-java/src/main/java/com/jimmy/learn/chapter10/thread/StopThread.java
@@ -0,0 +1,22 @@
+package com.jimmy.learn.chapter10.thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class StopThread {
+    private static boolean stopRequested;
+    public static void main(String[] args) throws InterruptedException {
+        Thread backgroundThread =  new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i=0;
+                while (!stopRequested){
+                    i++;
+                }
+            }
+        });
+        backgroundThread.start();
+        TimeUnit.SECONDS.sleep(1);
+        stopRequested = true;
+    }
+
+}
